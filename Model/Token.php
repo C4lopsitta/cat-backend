0a4d76b797009a3ad82b74e3c5898c09a1a637d2
Pollo133 <giuly.vad3@gmail.com>
2024-12-19 13:03:41 +0100

    Add classes User, Token and Cat. I refactored the project into folders.

---
+++ Model/Token.php
@@ -0,0 +1,30 @@
+<?php
+class Token{
+    private string $token;
+    private int $expirationDate;
+
+    /**
+     * @param string $token
+     * @param int $expirationDate
+     */
+    public function __construct(string $token, int $expirationDate) {
+        $this->token = $token;
+        $this->expirationDate = $expirationDate;
+    }
+
+    public function getToken(): string {
+        return $this->token;
+    }
+
+    public function setToken(string $token): void {
+        $this->token = $token;
+    }
+
+    public function getExpirationDate(): int {
+        return $this->expirationDate;
+    }
+
+    public function setExpirationDate(int $expirationDate): void {
+        $this->expirationDate = $expirationDate;
+    }
+}
