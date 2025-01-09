155fc844e63975c3f72e1b3fb2ffb3752932207d
ddaanii <torxdani1@gmail.com>
2025-01-09 12:23:28 +0100

    Added CartDAO.php class and implemented create methods

---
+++ DAO/CartDAO.php
@@ -0,0 +1,54 @@
+<?php
+
+class CartDAO extends GenericDAO
+{
+
+    public function create(object $object): ?object
+    {
+        try{
+            $sql = "INSERT INTO cartItems(uid, owner, cat)
+                        VALUES (:id, :owner, :cat)";
+
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute([
+                ':id' => UUID.genUuid(),
+                ':owner' => $object->getOwner(),
+                ':cat' => $object->getCat()
+            ]);
+
+            $id = $this->pdo->lastInsertId();
+            $object->setUid($id);
+
+            return $object;
+        }catch (PDOException $e){
+            echo($e->getMessage());
+            return null;
+        }
+    }
+
+    public function read(int $id): ?object
+    {
+        throw new Exception('Not implemented');
+    }
+
+    public function readAll(): ?array
+    {
+        throw new Exception('Not implemented');
+    }
+
+    public function readAllCartItems(): ?array
+    {
+
+        //TODO: Implement readAllCartItems
+    }
+
+    public function update(object $object): bool
+    {
+        throw new Exception('Not implemented');
+    }
+
+    public function delete(int $id): bool
+    {
+        // TODO: Implement delete() method.
+    }
+}
