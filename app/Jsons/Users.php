ec56c8fda63347f7e143b5d78b3c1509f61edcf7
Simone Robaldo <simone@atomtech.cc>
2025-01-14 23:58:33 +0100

    added mock users endpoint

--- app/Jsons/Users.php
+++ app/Jsons/Users.php
@@ -2,29 +2,32 @@
 
 namespace Jsons;
 
+use Utilities\Uid;
+
 class Users {
     static function listUsers(array $users): string {
         $usersJsonList = [];
 
         foreach ($users as $user) {
+            $uid = Uid::generate();
             $usersJsonList[] = <<< JSON
 {
-    "uid": "ffffffff-ffff-ffff-ffff-ffffffffffff",
-    "username": "Bob",
-    "image": "base64 ........",
-    "imageMime": "image/gif",
-    "description": "I'm bob and i love cats. I live in catworld and have 2^64 cats in my house. Their names are the fibonacci sequence. Except cat number 42 whose name is Megatron.",
-    "pronouns": "hee/hee",
-    "cats": [
-      {
-        "descr": "the father",
-        "value": 1000
-      }
-    ],
-    "wishlist": [
-      "ffffffff-ffff-ffff-ffff-ffffffffffff"
-    ]
-}
+        "uid": "{$uid}",
+        "username": "Bob",
+        "image": "base64 ........",
+        "imageMime": "image/gif",
+        "description": "I'm bob and i love cats. I live in catworld and have 2^64 cats in my house. Their names are the fibonacci sequence. Except cat number 42 whose name is Megatron.",
+        "pronouns": "hee/hee",
+        "cats": [
+          {
+            "descr": "the father",
+            "value": 1000
+          }
+        ],
+        "wishlist": [
+          "ffffffff-ffff-ffff-ffff-ffffffffffff"
+        ]
+    }
 JSON;
         }
 
