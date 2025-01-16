b249937aee6f0e9532acd9177e96475f2df2fa06
c4lopsitta <simone@atomtech.cc>
2025-01-16 12:08:11 +0100

    Added redis

--- app/DAO/Redis.php
+++ app/DAO/Redis.php
@@ -22,6 +22,11 @@ class RedisDb {
         }
     }
 
+    /**
+     * Generates a token to send to the user for account verification that expires after 24 hours.
+     * @param string $userUid
+     * @return string
+     */
     static public function generateAndStoreAccountConfirmToken(string $userUid): string {
         $accountConfirmToken = hash('sha224', $userUid . rand(100000, 999999));
 
@@ -32,6 +37,22 @@ class RedisDb {
         self::$instance->setex("confirmToken:$accountConfirmToken", 60 * 60 * 24, $userUid);
 
         return $accountConfirmToken;
+    }
+
+    /**
+     * Verifies the validity of the user token, if valid it will delete it from the Redis Instance and return the user's UID, otherwise it will return Null;
+     * @param string $accountConfirmToken
+     * @return string|null
+     */
+    static public function verifyAccountConfirmToken(string $accountConfirmToken): ?string {
+        if(self::$instance == null) {
+            throw new RedisException('Redis connection not established');
+        }
+
+        $userUid = self::$instance->get("confirmToken:".$accountConfirmToken);
+        self::$instance->del("confirmToken:".$accountConfirmToken);
+
+        return $userUid;
     }
 
 }
