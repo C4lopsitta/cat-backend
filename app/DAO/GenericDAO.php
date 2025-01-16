c043e6037486c755175856eac3ec8c42ba6f294c
ddaanii <torxdani1@gmail.com>
2025-01-16 12:20:13 +0100

    Fixed Bugs on GenericDAO.php

--- app/DAO/GenericDAO.php
+++ app/DAO/GenericDAO.php
@@ -7,30 +7,28 @@ use PDO;
 abstract class GenericDAO {
   protected static ?PDO $pdo = null;
 
-  public function __construct() {}
-
-  public function connect(): bool {
-    if ($this->pdo != null) return false;
+  public static function connect(): bool {
+    if (self::$pdo != null) return false;
     $connectionString = "mysql:host=db;port=3306;dbname=" . getenv("DB_NAME") . ";charset=utf8mb4";
 
-    $this->pdo = new PDO($connectionString, getenv('DB_USER'), getenv('DB_PASSWD'));
-    $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    self::$pdo = new PDO($connectionString, getenv('DB_USER'), getenv('DB_PASSWD'));
+    self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return true;
   }
 
-  public function disconnect() {
-    $this->pdo = null;
+  public static function disconnect() {
+    self::$pdo = null;
   }
 
-  abstract public function create(object $object): ?object;
+  abstract static public function create(object $object): ?object;
 
-  abstract public function read(int $id);
+  abstract static public function read(int $id);
 
-  abstract public function readAll(): ?array;
+  abstract static public function readAll(): ?array;
 
-  abstract public function update(object $object): bool;
+  abstract static public function update(object $object): bool;
 
-  abstract public function delete(int $id): bool;
+  abstract static public function delete(int $id): bool;
 }
 
 ?>
