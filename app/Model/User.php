b83cdaeb7b095d52c82ec7a551efa62e09121d6b
Pollo133 <111647806+Pollo133@users.noreply.github.com>
2025-01-16 12:58:29 +0100

    Database update for users
    * Add confirmation bool to user #15 and Drop tokens table in MariaDB #14
    
    * Add image with mime type to users #11
     Add     imageMime VARCHAR(16),
             description TEXT,
             pronouns VARCHAR(32) to table users
    Add     imageMime VARCHAR(16) in table cats
    
    * Add image with mime type to users #11
     Add     imageMimeType VARCHAR(16),
             description TEXT,
             pronouns VARCHAR(32) to table users
    Add     imageMimeType VARCHAR(16) in table cats
    
    * Add ImageMimeType in class Cat
    
    * Added race in class Cat
    Added imageMimeType, description, pronouns

--- app/Model/User.php
+++ app/Model/User.php
@@ -6,20 +6,34 @@ class User
     private string $uid;
     private string $username;
     private string $email;
+    private ?string $image;
+    private ?string $imageMimeType;
+    private ?string $description;
+    private ?string $pronouns;
     private string $passwordHash;
+    private bool $isAccountConfirmed;
 
     /**
-     * @param string $uid
      * @param string $username
+     * @param string $uid
      * @param string $email
+     * @param string|null $image
+     * @param string|null $imageMimeType
+     * @param string|null $description
+     * @param string|null $pronouns
      * @param string $passwordHash
+     * @param bool $isAccountConfirmed
      */
-    public function __construct(string $uid, string $username, string $email, string $passwordHash)
-    {
-        $this->uid = $uid;
+    public function __construct(string $username, string $uid, string $email, ?string $image, ?string $imageMimeType, ?string $description, ?string $pronouns, string $passwordHash, bool $isAccountConfirmed) {
         $this->username = $username;
+        $this->uid = $uid;
         $this->email = $email;
+        $this->image = $image;
+        $this->imageMimeType = $imageMimeType;
+        $this->description = $description;
+        $this->pronouns = $pronouns;
         $this->passwordHash = $passwordHash;
+        $this->isAccountConfirmed = $isAccountConfirmed;
     }
 
     public function getUid(): string
@@ -61,5 +75,47 @@ class User
     {
         $this->passwordHash = $passwordHash;
     }
+
+    public function getImage(): ?string {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): void {
+        $this->image = $image;
+    }
+
+    public function getImageMimeType(): ?string {
+        return $this->imageMimeType;
+    }
+
+    public function setImageMimeType(?string $imageMimeType): void {
+        $this->imageMimeType = $imageMimeType;
+    }
+
+    public function getDescription(): ?string {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): void {
+        $this->description = $description;
+    }
+
+    public function getPronouns(): ?string {
+        return $this->pronouns;
+    }
+
+    public function setPronouns(?string $pronouns): void {
+        $this->pronouns = $pronouns;
+    }
+
+    public function isAccountConfirmed(): bool {
+        return $this->isAccountConfirmed;
+    }
+
+    public function setIsAccountConfirmed(bool $isAccountConfirmed): void {
+        $this->isAccountConfirmed = $isAccountConfirmed;
+    }
+
+
 
 }
